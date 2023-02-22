fee858818f5ec2c97b4d7c977e083f42f61b943a
Steven Levy <slevy399@yahoo.com>
2023-02-21 22:24:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+
+const officeLoc = 42
+
+function distanceFromHqInBlocks(someValue) {
+    if(someValue < officeLoc){
+        return (officeLoc - someValue)
+    }
+    else{
+        return (someValue - officeLoc)
+    }
+  }
+
+function distanceFromHqInFeet(someValue){
+    let distanceInBlocks = distanceFromHqInBlocks(someValue);
+    let distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(destination > start){
+        return(destination - start) * 264
+    }
+    else{
+        return(start - destination) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceTravelled = distanceTravelledInFeet(start, destination);
+    if(distanceTravelled > 2500){
+        return ("cannot travel that far")
+    }
+    else if(distanceTravelled > 2000){
+        return 25
+    }
+    else if(distanceTravelled > 400){
+        return (distanceTravelled - 400) * 0.02
+    }
+    else if(distanceTravelled <= 400){
+        return 0
+    }
+}
